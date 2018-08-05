a3a90331f5a42052e780244f562547e122e1cd1a
kate-young <S1391628@student.mcckc.edu>
2018-08-05 14:24:34 -0500

    Add axis labels

--- data/transform.py
+++ data/transform.py
@@ -14,11 +14,20 @@ def getCompoundIndex(row):
 
 predictions.loc[:, "compound_i"] = predictions["Name"].apply(getCompoundIndex)
 
-#predictions = predictions[np.abs(predictions["value"]) < 100]
+predictions = predictions[(predictions["value"] < 200) & (predictions["value"] > -100)]
 predictions = predictions.values.tolist()
+
+ftr_data = pd.read_csv("features.csv").set_index("feature")
+models = list(ftr_data.columns)
+feature_counts = ftr_data.reset_index().values.tolist()
+features = {}
+for m in models:
+    ftrs = list(ftr_data.loc[ftr_data[m] > 0, m].index.values)
+    features[m] = ftrs
 
 with open("predictions.json", 'w') as outfile:
     json.dump({"compounds": compounds,
         "models": models,
-        "predictions": predictions
+        "predictions": predictions,
+        "features": features
     }, outfile)
